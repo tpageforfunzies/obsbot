e2f0f0587c7ee144b8ca7ae2fb74dcbba2c939a2
tpage <taylor.jay.page@gmail.com>
2021-02-03 20:27:36 -0600

    added image url post route

--- server.py
+++ server.py
@@ -12,5 +12,14 @@ def hello():
 def carousel():
     return "carousel template will go here"
 
+@app.route('/addimage', methods=['POST'])
+def add_image():
+    if not request.method == 'POST':
+        return('get the fuck outta here that that shit')
+
+    image_url = request.form.get('image_url')
+    print(image_url)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
