ca5b93c0591ee9bcc32fa140a83939f15a39b051
tpage <taylor.jay.page@gmail.com>
2021-02-03 20:20:39 -0600

    server stands up on aws host

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
 # server here
 # pip install flask
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/carousel')
+def hello():
+    return "carousel template will go here"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
